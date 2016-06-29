1d667dda3a50d8928712e43cec9c716891690173
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-06-29 22:39:31 +0300

    angular-material integrate

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 'use strict';
 
+import "./index.scss";
 import angular from "angular";
+import "angular-animate";
+import "angular-aria";
+import "angular-material";
 import uirouter from "angular-ui-router";
 import routing from "./app.routing";
 import home from "./components/home";
 
 angular
   .module('myApp', [
+    'ngMaterial',
     uirouter,
     home
   ])
