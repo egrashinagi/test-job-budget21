8f18085e209187de532679019280b5d34bb96cc8
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-06-30 16:54:48 +0300

    move data to controller

--- src/components/main/main.routes.js
+++ src/components/main/main.routes.js
@@ -5,6 +5,19 @@ export default function routes($stateProvider) {
   $stateProvider
     .state('main', {
       url: '/main',
-      template: require('html!./main.html')
+      template: require('html!./main.html'),
+      resolve: {
+        simpleObj() {
+          return {value: 'simple!'};
+        }
+      },
+      controller($scope){
+        $scope.menuItems = [
+          {title: 'home', state: 'home'},
+          {title: 'about', state: 'main'},
+          {title: 'sample post', state: 'main'},
+          {title: 'contacts', state: 'main'}
+        ];
+      }
     });
 }
