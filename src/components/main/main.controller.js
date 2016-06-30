cc40ccde5c6b4c1f8d80c15f1f8ca11ee9533ddd
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-06-30 17:19:33 +0300

    change controller

--- src/components/main/main.controller.js
+++ src/components/main/main.controller.js
@@ -1,10 +1,11 @@
 'use strict';
 export default class MainController {
-  constructor() {
-    this.name = 'myHome';
-  }
-
-  changeName() {
-    this.name = 'newName';
+  constructor($scope) {
+    $scope.menuItems = [
+      {title: 'home', state: 'home'},
+      {title: 'about', state: 'main'},
+      {title: 'sample post', state: 'main'},
+      {title: 'contacts', state: 'main'}
+    ];
   }
 }
